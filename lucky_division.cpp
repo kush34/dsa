3c7caedd478bd52b7410e606fda9f5e1a8169711
kush34 <chattkush34@gmail.com>
2026-04-21 11:02:40 +0530

    new

---
+++ lucky_division.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int num;
+    cin >> num;
+    int luckyNumber[14] = {4, 7, 44, 47, 74, 77, 444, 447, 474, 477, 744, 747, 774, 777};
+    // lucky Number: contains digit that contains only 4 and 7
+    // check if number is lucky
+
+    // Almost lucky number : evenly divied by some lucky Number
+
+    // ex 47 is lucky number and can be divided by ?
+    bool isFound = false;
+    for (int i : luckyNumber)
+    {
+        if (num % i == 0)
+        {
+            isFound = true;
+            break;
+        }
+    }
+    cout << (isFound ? "YES" : "NO") << endl;
+}
